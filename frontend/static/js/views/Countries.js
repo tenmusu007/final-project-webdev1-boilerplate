cb3aa7b08d1cd802a58723c67089213c0fbf7e73
Atsuya Tanaka <tenmusu007@gmail.com>
2022-05-16 09:13:44 -0700

    atsu

--- frontend/static/js/views/Countries.js
+++ frontend/static/js/views/Countries.js
@@ -7,6 +7,26 @@ export default class extends AbstractView{
     }
 
     async getHTML() {
-        return "<h1>Countries</h1>";
+        console.log(this.params.id);
+        const div = document.createElement("div");
+        const cont = document.getElementById("container")
+        const box =document.createElement("div")
+        cont.appendChild(box);
+        box.innerHTML =`<h1> atsu </h1>`
+        div.innerHTML = `
+        <p id ="ptag">This is content</p>
+        `
+        return `
+        <div id= "container"> 
+
+        </div>
+        `
     }
 }
+// const cont = document.getElementById("container")
+// const box =document.createElement("div")
+// cont.appendChild(box);
+// box.innerHTML =`<h1> atsu </h1>`
+
+// const item = localStorage.getItem('countries')
+// console.log(item)
