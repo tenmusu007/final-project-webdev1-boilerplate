60679a13b12f45e8f9057cb749134ba2ded169e1
Vini Guimarães <niciusfg@gmail.com>
2022-05-18 10:01:15 -0700

    homepage in js

--- frontend/static/js/views/Homepage.js
+++ frontend/static/js/views/Homepage.js
@@ -1,4 +1,18 @@
 import AbstractView from "./AbstractView.js";
+import { countries } from "../index.js"; 
+
+const getFlag = function(country) {
+    let name = "";
+    let flag = "";
+    for (let i = 0 ; i <  countries.length ; i++) {
+        if (countries[i].cca3.toLowerCase() ===  country.toLowerCase() ){
+            name = countries[i].name.common;
+            flag = countries[i].flags.png;
+        }
+    }
+    return flag;
+  };
+  console.log(getFlag("bra"));
 
 export default class extends AbstractView{
     constructor(params) {
@@ -7,8 +21,41 @@ export default class extends AbstractView{
     }
 
     async getHTML() {
-        return `<h1>Homepage</h1>`
+        return `<!-- MAINBANNER -->
+        <div class="container-fluid" id="mainBanner">
+            <div class="row align-items-center">
+                <div class="col-xs-12 col-md-6">
+                    <h1>Countries<br><span>Informations</span></h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, nobis eius fugit dignissimos, accusamus deleniti quis corporis quam excepturi optio, explicabo quos est harum vitae ducimus aut perspiciatis assumenda. Odit?</p>
+                    <button><a href="#">See Countries</a></button>
+                </div>
+            </div>
+        </div>
+        <!-- TEAM -->
+        <div class="container-fluid" id="team">
+            <h2>Our Team</h2>
+            <div class="row justify-content-center flags">            
+                <div class="col-12 col-md-3">
+                    <a href="http://localhost:3000/countries/bra">
+                        <img class="img-fluid" src="${getFlag("bra")}" alt="Brazil">
+                    </a>
+                    <span>From Brazil: <br>
+                        Clelia | Vinicius</span>
+                </div>
+                <div class="col-12 col-md-3">
+                     <a href="http://localhost:3000/countries/bra">
+                      <img class="img-fluid" src="${getFlag("jpn")}" alt="">
+                    </a>
+                    <span>From Japan:<br> 
+                        Atsuya</span>
+                </div>
+            </div>
+            <div class="row bgColors">
+                <div class="col-6 redBg"></div>
+                <div class="col-6 greemBg"></div>
+            </div>
+        </div>
+        `
         ;
-
     }
 }
